34f225b084a66b78f50214af32ee3e728bcbb0d6
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-08-01 13:06:58 +0200

    refactor(components): Reformatted file

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,11 +15,13 @@ interface ButtonPropsI extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const style = {
-  common: "box-border rounded border px-4 py-2 font-medium leading-5 transition-colors",
+  common:
+    "box-border rounded border px-4 py-2 font-medium leading-5 transition-colors",
   commonDisabled: "cursor-default border-dark-400 text-dark-200",
   solid: "bg-primary border-primary text-white",
   solidDisabled: "bg-dark-400",
-  outlined: "border-opacity-70 border-primary text-primary hover:border-opacity-100",
+  outlined:
+    "border-opacity-70 border-primary text-primary hover:border-opacity-100",
   outlinedDisabled: "",
   text: "border-transparent text-primary",
   textDisabled: "border-transparent text-dark-200",
@@ -62,13 +64,13 @@ export function Button({
       >
         <span className={"flex items-center justify-center gap-2"}>
           {iconLeft && iconLeft}
-          <p
+          <div
             className={clsx({
-              "w-full text-center": fullWidth,
+              "flex w-full justify-center": fullWidth,
             })}
           >
             {children}
-          </p>
+          </div>
           {iconRight && iconRight}
         </span>
       </IconContext.Provider>
