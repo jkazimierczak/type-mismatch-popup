33b9ab45a8c2756d691e620a852d7b2ce04ccec1
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-08-20 13:47:55 +0200

    build(deps): Initialized shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)"],
@@ -22,7 +29,21 @@ export default {
           900: "#181a1b",
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
