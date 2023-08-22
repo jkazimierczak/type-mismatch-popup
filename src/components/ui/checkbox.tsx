6013e328437ddbfdafe1d76bb343805797ed397d
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-08-22 17:12:10 +0200

    feat(components): Added shadcn/ui components

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+
+import { cn } from "@/utils";
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className="flex h-5 w-5 appearance-none items-center justify-center rounded-[4px] border data-[state=checked]:border-none data-[state=checked]:bg-primary"
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("flex items-center justify-center text-current")}
+    >
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
